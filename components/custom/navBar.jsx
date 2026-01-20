b405c9c6809edb7038cd5dc77d4e4e698f38342e
Joshua Lopez <lopezjoshuapanaligan@gmail.com>
2026-01-20 22:53:08 +0800

    landing page init draft

--- components/custom/navBar.jsx
+++ components/custom/navBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
         <div className="flex items-center justify-between h-16 p-4 pl-40 pr-40 shadow-lg bg-white sticky top-0 z-99 w-full">
             <div className="flex items-center justify-center gap-2">
                 <Logo />
-                <p className="font-serif font-bold  ">ElderKey</p>
+                <p className="font-serif font-bold text-deep-navy">ElderKey</p>
             </div>
             <div className="flex items-center justify-center gap-2">
                 <Button variant="ghost" className={"text-black cursor-pointer"}>
